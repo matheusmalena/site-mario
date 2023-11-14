23028d6436c5637dba9ed570c6576d85e88f657c
Matheus Malena <matheusmalena28@gmail.com>
2023-11-14 16:55:17 -0300

    implementação de audio

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
-let formulario = document.querySelector(".fale-conosco")
-let mascara = document.querySelector(".mascara-form")
+let formulario = document.querySelector(".fale-conosco");
+let mascara = document.querySelector(".mascara-form");
+let somClique = document.querySelector(".somClique");
+let fundoAudio = document.querySelector('.somFundo');
 
+document.body.addEventListener('click', function () {
+    // Obtém o elemento de áudio pelo nome de classe
+    var audioElement = document.querySelector('.somFundo');
 
-function cliqueiNoBotao(){
-    formulario.style.left = "700px"
-    mascara.style.visibility = "visible"
+    // Inicia a reprodução do áudio
+    if (audioElement) {
+        audioElement.play();
+        audioElement.loop = true;
+    }
+    if (fundoAudio) {
+        // Reduz o volume em 10%
+        fundoAudio.volume = Math.max(0, fundoAudio.volume - 0.3);
+    }
+});
+
+function cliqueiNoBotao() {
+    formulario.style.left = "700px";
+    mascara.style.visibility = "visible";
+    somClique.play();
 }
 
-function sumirFormulario(){
-    formulario.style.left = "-320px"
-    mascara.style.visibility = "hidden"
+function sumirFormulario() {
+    formulario.style.left = "-320px";
+    mascara.style.visibility = "hidden";
 }
